1fe002887686cc2f444779bd391ca674b2e9e15f
mradzwilla <mradzwilla@gmail.com>
2020-06-29 02:38:44 -0400

    Completed user auth section and navbar

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios';
-import { Link } from 'react-router-dom';
 
-const Login = () => {
+// import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
+
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -14,8 +18,14 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log('Success')
+    login(email, password)
   }
+
+  //Redirect if logged in
+  if(isAuthenticated){
+    return <Redirect to="/dashboard" />
+  }
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign In</h1>
@@ -50,4 +60,13 @@ const Login = () => {
   )
 }
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login)
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { register } from '../../actions/auth';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -17,11 +21,17 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2){
-      console.log('Passwords do not match')
+      setAlert('Passwords do not match', 'danger')
     } else {
-      console.log('Success')
+      register({name, email, password});
     }
   }
+
+  //Redirect if logged in
+  if(isAuthenticated){
+    return <Redirect to="/dashboard" />
+  }
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign Up</h1>
@@ -34,7 +44,6 @@ const Register = () => {
               name="name"
               value={name}
               onChange={e => onChange(e)}
-              required
             />
           </div>
           <div className="form-group">
@@ -44,7 +53,6 @@ const Register = () => {
               name="email"
               value={email}
               onChange={e => onChange(e)}
-              required
             />
             <small className="form-text"
               >This site uses Gravatar so if you want a profile image, use a
@@ -56,7 +64,6 @@ const Register = () => {
               type="password"
               placeholder="Password"
               name="password"
-              minLength="6"
               value={password}
               onChange={e => onChange(e)}
             />
@@ -66,7 +73,6 @@ const Register = () => {
               type="password"
               placeholder="Confirm Password"
               name="password2"
-              minLength="6"
               value={password2}
               onChange={e => onChange(e)}
             />
@@ -80,4 +86,14 @@ const Register = () => {
   )
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
