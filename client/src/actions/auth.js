b1ed18439be61e24ed7660b9593aa283f5c191c6
mradzwilla <mradzwilla@gmail.com>
2020-07-03 11:56:28 -0400

    Completed dashboard. Users can now add and removed experiences and education as well as delete their entire account

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken'
 
@@ -52,7 +53,7 @@ export const register = ({ name, email, password }) => async dispatch => {
     dispatch(loadUser());
 
   } catch (err) {
-    const errors = err.response.data.errors;
+    const errors = err.data.errors;
 
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
@@ -98,5 +99,6 @@ export const login = (email, password) => async dispatch => {
 
 //Logout / Clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 }
